4352a1d78615bf5e5c82743f03303d6fe668e7c3
JoyD-0303 <148120294+JoyD-0303@users.noreply.github.com>
2025-04-03 10:21:25 -0700

    Create Deck Class

--- src/main/java/model/Deck.java
+++ src/main/java/model/Deck.java
@@ -1,5 +1,41 @@
 package model;
 
-public class Deck {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Deck {
+	private ArrayList<Card> cards;
+
+	/* CONSTRUCTORS */
+	public Deck() {
+		cards = new ArrayList<Card>();
+		for (Suit s : Suit.values()) {
+			for (Rank r : Rank.values()) {
+				Card c = Card.get(r, s);
+				cards.add(c);
+			}
+		}
+	}
+	/* METHODS */
+
+	// Remove card from top of Deck
+	public Card pop() {
+		return cards.remove(0);
+	}
+
+	// Shuffle deck of Cards
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+
+	// Reset deck of cards
+	public void reset() {
+		cards.clear();
+		for (Suit s : Suit.values()) {
+			for (Rank r : Rank.values()) {
+				Card c = Card.get(r, s);
+				cards.add(c);
+			}
+		}
+	}
 }
