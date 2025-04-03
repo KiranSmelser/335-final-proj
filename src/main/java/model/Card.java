9ce07c3952ef1067c0fa83b05da304517e139a62
JoyD-0303 <148120294+JoyD-0303@users.noreply.github.com>
2025-04-03 10:22:15 -0700

    Create Card.java

---
+++ src/main/java/model/Card.java
@@ -0,0 +1,77 @@
+package model;
+
+public class Card implements Comparable<Card> {
+
+	private final Suit suit;
+	private final Rank rank;
+
+	/* PRIVATE CONSTRUCTORS */
+	private Card(Rank rank, Suit suit) {
+		this.rank = rank;
+		this.suit = suit;
+	}
+
+	/* PUBLIC STATIC GETTER */
+	public static Card get(Rank pRank, Suit pSuit) {
+		assert pRank != null && pSuit != null;
+		return CARDS[pSuit.ordinal()][pRank.ordinal()];
+	}
+
+	/* PRIVATE STATIC STORE */
+	private static final Card[][] CARDS = new Card[Suit.values().length][Rank.values().length];
+
+	static {
+		for (Suit suit : Suit.values()) {
+			for (Rank rank : Rank.values()) {
+				CARDS[suit.ordinal()][rank.ordinal()] = new Card(rank, suit);
+			}
+		}
+	}
+
+	public Rank getRank() {
+		return rank;
+	}
+
+	public Suit getSuit() {
+		return suit;
+	}
+
+	public int getValue() {
+		return rank.getValue();
+	}
+
+	public int compareTo(Card other) {
+		if (this.getValue() < other.getValue()) {
+			return -1;
+		} else if (this.getValue() > other.getValue()) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	public String toString() {
+		char suitIcon = '\u2663';
+		if (suit == Suit.DIAMONDS)
+			suitIcon = '\u2666';
+		if (suit == Suit.HEARTS)
+			suitIcon = '\u2665';
+		if (suit == Suit.SPADES)
+			suitIcon = '\u2660';
+
+		String value = this.getValue() + "";
+		if (rank == Rank.ACE) {
+			value = "A";
+		}
+		if (rank == Rank.KING) {
+			value = "K";
+		}
+		if (rank == Rank.QUEEN) {
+			value = "Q";
+		}
+		if (rank == Rank.JACK) {
+			value = "J";
+		}
+		return value + "" + suitIcon;
+	}
+}
