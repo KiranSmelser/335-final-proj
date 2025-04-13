0176be9d8067ad2131a1ae5e047fd0d00a08de1a
chanwooyoon1 <chanwooyoon@arizona.edu>
2025-04-12 19:34:40 -0700

    Update HardStrategy.java

--- src/main/java/model/player/HardStrategy.java
+++ src/main/java/model/player/HardStrategy.java
@@ -1,5 +1,16 @@
 package player;
 
+import java.util.List;
+
+import cribbage.Hand;
+import deck.Card;
+
 public class HardStrategy implements Strategy {
+
+	@Override
+	public List<Card> discard(Hand hand, boolean isDealer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
