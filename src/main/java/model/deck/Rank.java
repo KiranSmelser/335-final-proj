26fdfcc988fe540e56868cd1e73dd95eee1ffa4e
JoyD-0303 <148120294+JoyD-0303@users.noreply.github.com>
2025-04-13 15:24:40 -0700

    Update Rank.java
    - Bug fix Rank

--- src/main/java/model/deck/Rank.java
+++ src/main/java/model/deck/Rank.java
@@ -1,8 +1,7 @@
 package deck;
 
 public enum Rank {
-	DEUCE(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13),
-	ACE(1);
+	ACE(1), DEUCE(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10);
 
 	private int value;
 
