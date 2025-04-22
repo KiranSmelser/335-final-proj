3ee9fb5a3bb89f07dde58900b34d6a1aff6a5180
chanwooyoon1 <chanwooyoon@arizona.edu>
2025-04-21 17:37:38 -0700

    Update Strategy.java

--- src/main/java/model/player/Strategy.java
+++ src/main/java/model/player/Strategy.java
@@ -2,9 +2,10 @@ package player;
 
 import java.util.List;
 
+import cribbage.Board;
 import cribbage.Hand;
 import deck.Card;
 
 public interface Strategy {
-    List<Card> discard(Hand hand, boolean isDealer);
+    List<Card> discard(Hand hand, Board board);
 }
