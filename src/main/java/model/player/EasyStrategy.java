4e5f8034a8dcec760dd33abe5fa5ed3d09129bf1
chanwooyoon1 <chanwooyoon@arizona.edu>
2025-04-21 17:37:22 -0700

    Update EasyStrategy.java

--- src/main/java/model/player/EasyStrategy.java
+++ src/main/java/model/player/EasyStrategy.java
@@ -3,17 +3,17 @@ package player;
 import java.util.Collections;
 import java.util.List;
 
+import cribbage.Board;
 import cribbage.Hand;
 import deck.Card;
 
 public class EasyStrategy implements Strategy {
 
 	@Override
-	public List<Card> discard(Hand hand, boolean isDealer) {
-		List<Card> cards = hand.getCard(); // temp method
+	public List<Card> discard(Hand hand, Board board) {
+		List<Card> cards = hand.getCards();
 		Collections.shuffle(cards);
 		return cards.subList(0, 2);
-		return null;
 	}
 
 }
