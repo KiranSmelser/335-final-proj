e4209d00a84981e7945b979d8ba6f51683a9cbf8
jf80272 <166781698+jf80272@users.noreply.github.com>
2025-04-24 09:18:30 -0700

    Update ComputerPlayer.java

--- src/main/java/model/player/ComputerPlayer.java
+++ src/main/java/model/player/ComputerPlayer.java
@@ -1,5 +1,22 @@
 package player;
 
-public class ComputerPlayer {
+import java.util.List;
 
+import main.java.model.cribbage.Board;
+import main.java.model.deck.Card;
+
+public class ComputerPlayer extends Player {
+	private Strategy strategy;
+	private Board board;
+
+	public ComputerPlayer(String name, Strategy strategy, Board board) {
+		super(name);
+		this.strategy = strategy;
+		this.board = board;
+	}
+
+	@Override
+	List<Card> selectDiscards() {
+		return strategy.discard(getHand(), board);
+	}
 }
