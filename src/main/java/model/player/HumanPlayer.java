a5402544af8d675b34442c22441ab991be816d7f
jf80272 <166781698+jf80272@users.noreply.github.com>
2025-04-24 09:19:11 -0700

    Update HumanPlayer.java

--- src/main/java/model/player/HumanPlayer.java
+++ src/main/java/model/player/HumanPlayer.java
@@ -1,5 +1,25 @@
 package player;
 
-public class HumanPlayer {
+import java.util.ArrayList;
+import java.util.List;
 
+import main.java.model.deck.Card;
+
+public class HumanPlayer extends Player {
+	private List<Card> discards;
+
+	public HumanPlayer(String name) {
+		super(name);
+		discards = new ArrayList<Card>();
+	}
+
+	@Override
+	List<Card> selectDiscards() {
+		return discards;
+	}
+	
+	public void setDiscards(Card c1, Card c2) {
+		discards.add(c1);
+		discards.add(c2);
+	}
 }
