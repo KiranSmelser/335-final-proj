559d6f09517f985fe1d477069e069784f71a5b11
Kiran Smelser <kiran.smelser@gmail.com>
2025-04-25 15:23:40 -0700

    updated JUnit test suite to contain player tests

--- src/test/junit/model/ModelTests.java
+++ src/test/junit/model/ModelTests.java
@@ -4,6 +4,8 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({ CardTest.class, DeckTest.class, HandTest.class, CribTest.class, EasyStrategyTest.class, HardStrategyTest.class, CribbageTest.class, BoardTest.class })
+@SelectClasses({ CardTest.class, DeckTest.class, HandTest.class, CribTest.class, 
+	EasyStrategyTest.class, HardStrategyTest.class, CribbageTest.class, BoardTest.class, 
+	ComputerPlayerTest.class, PlayerTest.class })
 public class ModelTests {
 }
